f79d899201a332767415e84bc3161e3d702224a7
Max Williams <ss4max@gmail.com>
2023-04-09 15:40:04 +0700

    Users and authorization working

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const reservationsController = require('../controllers/reservationsController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
 
 router.route('/')
     .get(reservationsController.getAllReservations)
