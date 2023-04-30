43df1865a8b69e17a83e8f53546f6ec207690660
Max Williams <ss4max@gmail.com>
2023-04-30 15:41:06 +0700

    webhook url secured

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const transactionsController = require('../controllers/transactionsController')
-// const verifyJWT = require('../middleware/verifyJWT')
+const verifyJWT = require('../middleware/verifyJWT')
 
-// router.use(verifyJWT)
+router.use(verifyJWT)
 
 router.route('/')
     .get(transactionsController.getAllTransactions)
--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const webhooksController = require('../controllers/webhooksController')
-// const verifyJWT = require('../middleware/verifyJWT')
-
-// router.use(verifyJWT)
 
 router.route('/')
     .get(webhooksController.getAllWebhooks)
