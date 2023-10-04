5670027e2474de8f713637b15bfdb3e2ec914455
Max Williams <ss4max@gmail.com>
2023-10-04 22:44:55 +0700

    enabled jwt auth

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const roomsController = require('../controllers/roomsController')
-// const verifyJWT = require('../middleware/verifyJWT')
+const verifyJWT = require('../middleware/verifyJWT')
 
-// router.use(verifyJWT)
+router.use(verifyJWT)
 
 router.route('/')
     .get(roomsController.getAllRooms)
