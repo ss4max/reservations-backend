8ab5f0ecb49170b8c7a3894570c9f7702aac3951
Max Williams <ss4max@gmail.com>
2024-09-26 20:51:56 +0700

    deploy prep

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,7 @@ const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin: (origin, callback) => {
+        //remove !origin on deploy
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
