0e495fc41f032c289f064baa8cb8cd406a9e6783
Max Williams <ss4max@gmail.com>
2024-11-26 21:37:29 +0700

    allowed origins pulls from .env

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
+const domainURL = process.env.DOMAIN;
+
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3500',
     'https://checkout.stripe.com/c/pay/',
     'https://stripe.com',
-    'https://tothemaxstay.onrender.com',
+    domainURL
     // 'https://dandrepairshop.com'
 ]
 
