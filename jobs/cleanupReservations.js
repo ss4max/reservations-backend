0eaa8d0d096d9ebfefe4af89e4674a9672fa1d61
Max <ss4max@gmail.com>
2025-07-31 22:45:27 +0700

    refactor: restructure date handling functions and update cutoff time for expired reservations

--- jobs/cleanupReservations.js
+++ jobs/cleanupReservations.js
@@ -1,20 +1,64 @@
 const Reservation = require('../models/Reservation')
 const Room = require('../models/Room')
 
-const deleteExpiredPendingReservations = async () => {
-    const cutoff = new Date(Date.now() - 15 * 60 * 1000) // 15 minutes ago
+function createCheckedInDates(checkIn, checkOut) {
+    let dates = []
+    let newDate = new Date(checkIn)
+    while (checkIn.getTime() !== checkOut.getTime()) {
+        dates.push(newDate)
+        newDate = new Date(checkIn.setDate(checkIn.getDate() + 1))
+    }
+    return dates
+}
 
-    console.log('cutoff', cutoff)
+const getDatesArray = (datesArrayObj) => {
+    return datesArrayObj.map((obj) => obj.date);
+}
+
+function getDates(times) {
+    let tempArray = []
+    for (let i = 0; i < times.length; i++) {
+        tempArray[i] = new Date(times[i])
+    }
+    return tempArray
+}
+
+function deleteDates(datesArrayOfObjects, deleteTheseDates) {
+
+    const reservationId = datesArrayOfObjects[0]?.reservationId
+
+    //create an array of dates (no reservationId)
+    const dates = getDatesArray(datesArrayOfObjects)
+
+    //deletes dates of different time
+    const arrayOfDifferentTimes = getTimes(dates).filter((x) => !getTimes(deleteTheseDates).includes(x));
+
+    const arrayOfDates = getDates(arrayOfDifferentTimes)
+
+    //add reservationId back to each date
+    const datesOccupiedArrayOfObjects = arrayOfDates.map(date => tempObj = { date: date, reservationId: reservationId });
+
+    return datesOccupiedArrayOfObjects
+}
+
+function getTimes(datesArray) {
+    let tempArray = []
+    for (let i = 0; i < datesArray.length; i++) {
+        tempArray[i] = datesArray[i].getTime()
+    }
+
+    return tempArray
+}
+
+const deleteExpiredPendingReservations = async () => {
+    const cutoff = new Date(Date.now() - 30 * 60 * 1000) // 15 minutes ago
 
     const expiredReservations = await Reservation.find({
         paymentStatus: 'pending',
         createdAt: { $lt: cutoff }
     })
 
-    console.log('expiredReservations', expiredReservations)
-
     for (const reservation of expiredReservations) {
-        console.log(`Deleting expired reservation: ${reservation._id}`)
 
         // Cleanup room occupancy
         const roomModel = await Room.findOne({ roomName: reservation.room }).exec()
